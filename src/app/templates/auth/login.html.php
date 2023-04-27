f0013b80d3ca4b12b63604ab5ff20254942cc11c
tohya-ryu <ryu@tohya.net>
2023-04-27 13:43:36 +0200

    fix indentation

--- src/app/templates/auth/login.html.php
+++ src/app/templates/auth/login.html.php
@@ -1,26 +1,26 @@
 <div>
-    <b>LOGIN</b>
+  <b>LOGIN</b>
 </div>
 <div>
-    <form>
-        <div>
-            <label for="auth-email">E-Mail</label>
-            <input type="text" id="auth-email"/>
-        </div>
-        <div>
-            <label for="auth-password">Password</label>
-            <input type="password" id="auth-password"/>
-        </div>
-        <input type="hidden" id="csrf-token" name="csrf-token"
-          value="<?php echo $this->view->csrf_token ?>"/>
-        <div>
-            <button type="button" onclick="send_form()">Submit</button>
-        </div>  
-    </form>    
+  <form>
     <div>
-      <a href="<?php $this->base_uri('auth/signup'); ?>">Sign Up</a> <br>
-      <a href="
-        <?php $this->base_uri('auth/pwreset/apply'); ?>">I forgot my password
-      </a>
+      <label for="auth-email">E-Mail</label>
+      <input type="text" id="auth-email"/>
     </div>
+    <div>
+      <label for="auth-password">Password</label>
+      <input type="password" id="auth-password"/>
+    </div>
+    <input type="hidden" id="csrf-token" name="csrf-token"
+      value="<?php echo $this->view->csrf_token ?>"/>
+    <div>
+      <button type="button" onclick="send_form()">Submit</button>
+    </div>  
+  </form>    
+  <div>
+    <a href="<?php $this->base_uri('auth/signup'); ?>">Sign Up</a> <br>
+    <a href="
+      <?php $this->base_uri('auth/pwreset/apply'); ?>">I forgot my password
+    </a>
+  </div>
 </div>
