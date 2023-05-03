38427c3512e284a0ae461fd6962eef043fe8185f
tohya-ryu <ryu@tohya.net>
2023-05-03 11:47:01 +0200

    add routes

--- src/app/config/routes.conf.php
+++ src/app/config/routes.conf.php
@@ -2,6 +2,20 @@
 
 FrameworkRoute::set('get', '/', 'main#index');
 
+FrameworkRoute::set('post', 'count-words', 'main#count_words');
+FrameworkRoute::set('get', 'add/vocab', 'main#add_vocab');
+FrameworkRoute::set('get', 'add/kanji', 'main#add_kanji');
+FrameworkRoute::set('get', 'fetch/vocab/:search', 'main#fetch_vocab');
+FrameworkRoute::set('get', 'fetch/kanji/:search', 'main#fetch_kanji');
+FrameworkRoute::set('get', 'get/vocab/:id', 'main#vocab');
+FrameworkRoute::set('get', 'get/kanji/:id', 'main#kanji');
+FrameworkRoute::set('patch', 'update/vocab', 'main#update_vocab');
+FrameworkRoute::set('patch', 'update/kanji', 'main#update_kanji');
+FrameworkRoute::set('post', 'practice/kanji', 'main#practice_kanji');
+FrameworkRoute::set('post', 'practice/vocab', 'main#practice_vocab');
+FrameworkRoute::set('post', 'add/vocab', 'main#add_vocab_submit');
+FrameworkRoute::set('post', 'add/kanji', 'main#add_kanji_submit');
+
 require 'app/config/auth-routes.conf.php';
 
 
