fcd38ae3ffc303403bca97bfba8ba6cac35282e4
tohya-ryu <ryu@tohya.net>
2023-05-10 12:30:25 +0200

    update menu template

--- src/app/templates/_menu.html.php
+++ src/app/templates/_menu.html.php
@@ -2,8 +2,8 @@
 <div class="menu_container">
   <a href="<?php $this->base_uri('auth/logout') ?>">Logout</a> | 
   <a href="<?php $this->base_uri('') ?>">Home</a> | 
-  <a href="<?php $this->base_uri('add/vocab') ?>">Add Vocab</a> | 
-  <a href="<?php $this->base_uri('add/kanji') ?>">Add Kanji</a> | 
+  <a href="<?php $this->base_uri('new/vocab') ?>">New Vocab</a> | 
+  <a href="<?php $this->base_uri('new/kanji') ?>">New Kanji</a> | 
   <a href="<?php $this->base_uri('find') ?>">Find Data</a> | 
   <a href="<?php $this->base_uri('update/counters') ?>">Update</a> | 
 </div>
