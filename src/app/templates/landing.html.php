87b8ab26f9a750d53edd21c5db67f43e004411ae
tohya-ryu <ryu@tohya.net>
2023-05-22 13:30:15 +0200

    finish landing page style

--- src/app/templates/landing.html.php
+++ src/app/templates/landing.html.php
@@ -121,20 +121,23 @@ $ignore_month    = $time - 2419200;
 
 <!-- Kanji Form -->
 
-<div class="words_filter">
-  <span class="title">Kanji Filter</span>
+<div class="form-container">
   <form action="<?php $this->base_uri('practice/kanji') ?>" method="post" 
     accept-charset="utf-8">
 
+  <h2 class="title">Kanji Filter</h2>
+    
     <input type="hidden" name="csrf-token" 
       value="<?php echo $this->view->csrf_token ?>" />
     <input type="hidden" name="practice-start" value="true" />
     
-    <p>Look for a specific kanji</p>
+  <div class="input-container">
+    <label>Look for a specific kanji</label>
     <input name="search_by_kanji" value="" maxlength="10" size="10" />
-    <div class="line"></div>
+  </div>
 
-    <p>Order by</p>
+  <div class="input-container">
+    <label>Order by</label>
     <select name="order_rule">
       <option value="0" selected>Random</option>
       <option value="1">Success Rate (asc)</option>
@@ -142,9 +145,10 @@ $ignore_month    = $time - 2419200;
       <option value="3">Entry Date (asc)</option>
       <option value="4">Learn Date (desc)</option>
     </select>
-    <div class="line"></div>
+  </div>
 
-    <p>Limit by Learn Date (recently learned words are ignored)</p>
+  <div class="input-container">
+    <label>Limit by Learn Date (recently learned words are ignored)</label>
     <select name="ignore_latest">
       <option value="<?php echo $time ?>">Ignore none</option>
       <option value="<?php echo $ignore_recent ?>">Ignore last 6 hours</option>
@@ -156,41 +160,45 @@ $ignore_month    = $time - 2419200;
       </option>
       <option value="<?php echo $ignore_month ?>">Ignore last 4 weeks</option>
     </select>
-    <p>Or use custom time interval (yyyy-mm-dd hh:mm)</p>
-
-    <p>Use custom time interval: 
+  </div>
+  <div class="input-container">
+    <label>Use custom time interval</label>
     <input type="checkbox" name="custom_timespace" value="accept" />
     <select name="timespace_type">
       <option value="1" selected>Limit By Entry Date</option>
       <option value="2">Limit By Learn Date</option>
     </select></p>
-    <p>From
+    <div class="input-sub-container">From
     <?php $timespace = date("Y-m-d 00:00", $time-86400); ?>
     <input type="text" name="timespace_start" 
       value="<?php echo $timespace ?>" maxlength="16" size="16" />
     to 
     <?php $timespace = date("Y-m-d 00:00", $time); ?>
     <input type="text" name="timespace_end" 
-      value="<?php echo $timespace ?>" maxlength="16" size="16" /></p>
-    <div class="line"></div>
+      value="<?php echo $timespace ?>" maxlength="16" size="16" /></div>
+  </div>
     
-    <p>Learned no more than <i>x</i> times (set 0 to fetch unlearned words)</p>
+  <div class="input-container">
+    <label>Learned no more than <i>x</i> times (set 0 to fetch unlearned words)</label>
     <input type="text" name="counter_limit" value="9999" maxlength="11" 
       size="10" />
-    <div class="line"></div>
+  </div>
 
-    <p>Number of associated words (min - max)</p>
+  <div class="input-container">
+    <label>Number of associated words (min - max)</label>
     <input type="text" name="min_words" value="0" maxlength="11" size="10" />
     <input type="text" name="max_words" value="20000" maxlength="11" 
       size="10" />
-    <div class="line"></div>
+  </div>
 
-    <p>Maximum Success Rate</p>
+  <div class="input-container">
+    <label>Maximum Success Rate</label>
     <input type="text" name="success_limit" value="100" maxlength="3" 
       size="3"/>
-    <div class="line"></div>
+  </div>
 
-    <p>JLPT Filter</p>
+  <div class="input-container">
+    <label>JLPT Filter</label>
     <select name="jlpt">
       <option value="0" selected>Filter Off</option>
       <option value="1">N1</option>
@@ -199,13 +207,14 @@ $ignore_month    = $time - 2419200;
       <option value="4">N4</option>
       <option value="5">N5</option>
     </select>
-    <div class="line"></div>
+  </div>
 
-    <p>Tags (custom filters)</p>
+  <div class="input-container">
+    <label>Tags</label>
     <input type="text" name="custom" value="" maxlength="120" size="80" />
-    <div class="line"></div>
+  </div>
 
-    <p><input type="submit" name="form-submit" value="練習開始" /></p>
+    <p><input class="button" type="submit" name="form-submit" value="練習開始" /></p>
     
   </form>
 </div>
